147d71012de825a447b8231293b265a6d335b915
村上学 <>
2024-07-14 17:30:13 +0900

    店舗リスト

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from app.models import Store
+from django.views.generic import View
+from django.shortcuts import render
 
+
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        })
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "app/index.html"
